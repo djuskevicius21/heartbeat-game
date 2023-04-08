b435144fa1e500dd3916052f2b1037564cd56416
Daniel Juskevicius <jusk0003@algonquinlive.com>
2023-04-08 09:53:04 -0400

    Added scaffold for DatabaseIdentityStoreDefinition

--- src/main/java/cst8218/jusk0003/heartbeat/JAXRSConfiguration.java
+++ src/main/java/cst8218/jusk0003/heartbeat/JAXRSConfiguration.java
@@ -1,5 +1,9 @@
 package cst8218.jusk0003.heartbeat;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import javax.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
+import javax.security.enterprise.identitystore.PasswordHash;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -7,6 +11,16 @@ import javax.ws.rs.core.Application;
  * Configures JAX-RS for the application.
  * @author Juneau
  */
+/*@DatabaseIdentityStoreDefinition(
+   dataSourceLookup = "${'java:comp/DefaultDataSource'}",
+   callerQuery = "#{'select password from app.appuser where username = ?'}",
+   groupsQuery = "select groupname from app.appuser where username = ?",
+   hashAlgorithm = PasswordHash.class,
+   priority = 10
+)
+@ApplicationScoped
+@Named
+*/
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
     
